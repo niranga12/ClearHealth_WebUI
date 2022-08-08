288a17beb25a381f8fbb6d10a8acea2176ac8070
Mike Over <mike.over@clearhealthinc.com>
2022-08-08 08:42:29 -0400

    Initialize GA in testMode when not on production domain

--- src/_helpers/GoogleAnalytics.js
+++ src/_helpers/GoogleAnalytics.js
@@ -3,17 +3,20 @@ import ReactGA from 'react-ga4'
 import { useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-ReactGA.initialize('G-BMRB78ZB3Q')
-ReactGA.send('pageview')
-
 export default function GoogleAnalytics() {
   const location = useLocation()
   const loginState = useSelector((state) => state.Login)
 
   useEffect(() => {
+    const testMode = window.location.hostname !== 'my.clearhealthinc.com'
+    ReactGA.initialize('G-BMRB78ZB3Q', { testMode })
+    ReactGA.send('pageview')
+  }, [])
+
+  useEffect(() => {
     const currentPath = location.pathname + location.search
     ReactGA.send({ hitType: 'pageview', page: currentPath })
-  }, [location])
+  }, [location.pathname, location.search])
 
   useEffect(() => {
     ReactGA.set({
