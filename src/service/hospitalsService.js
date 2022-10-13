479354af4e0f3cb1da53a7e739bacd8f7052232c
Ajithan <ajithan@champitsolutions.com>
2022-10-13 10:26:11 +0530

    Changes

--- src/service/hospitalsService.js
+++ src/service/hospitalsService.js
@@ -1,6 +1,9 @@
 import axiosInstance from '../_helpers/axiosinstance'
 
 export const getHospitalsList = (data) => axiosInstance.post(`hospital/list`, data)
+
+export const getHealthSystemHospitalsList = (data) => axiosInstance.post(`hospital/list`, data)
+
 export const getHospitalsListCount = (data) => axiosInstance.post(`hospital/count`, data)
 export const saveHospital = (data) => axiosInstance.post(`hospital`, data)
 export const updateHospitalByPartyRoleId = (partyRoleId, data) => axiosInstance.put(`hospital/${partyRoleId}`, data)
